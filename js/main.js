a7fc523e36aa69d68fd9ec4f549d14a2125731e2
Angel Cruz <93997030+CruzAngell@users.noreply.github.com>
2023-11-01 21:55:20 -0500

    Modificaciones 1
    Se añade a la web el background tipo degradado y el circulo que sigue al cursor, además de añadir la animación al momento de hacer un clic en la web.

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var cursor = document.querySelector(".cursor");
+
+document.addEventListener("mousemove",function(e){
+    cursor.style.cssText = "left: " + e.clientX + "px; top: " +e.clientY + "px;";
+});
